6651cdf95a6022b84697ee5c992155e46370460a
Olga Leonteva <leonteva.h@gmail.com>
2016-02-24 15:36:30 +0300

    Типы переменных int, double, String

--- sandbox/src/main/java/ru/stqa/course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/MyFirstProgram.java
@@ -2,6 +2,12 @@ package ru.stqa.course.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 8;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + "= " + s);
+
     }
 }
