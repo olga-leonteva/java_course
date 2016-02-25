6885d0045e69131338fb9becefddaf9e6bee099f
Olga Leonteva <leonteva.h@gmail.com>
2016-02-25 13:02:47 +0300

    Добавлено несколько классов

---
+++ sandbox/src/main/java/ru/stqa/course/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.course.sandbox;
+
+/**
+ * Created by leonto on 2/24/2016.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/course/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.course.sandbox;
+
+/**
+ * Created by leonto on 2/24/2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+}
