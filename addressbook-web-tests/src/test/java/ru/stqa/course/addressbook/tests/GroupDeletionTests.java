6f49b75263f5bcb823e72f104fd7cab0dbcbec4b
Olga Leonteva <leonteva.h@gmail.com>
2016-03-03 13:26:40 +0300

    Создана 2х уровневая архитектура тестов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/GroupDeletionTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.course.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+
+public class GroupDeletionTests extends TestBase{
+
+    @Test
+    public void testGroupDeletion() {
+        app.gotoGroupPage();
+        app.selectGroup();
+        app.deleteSelectedGroups();
+        app.reternToGroupPage();
+
+    }
+
+}
