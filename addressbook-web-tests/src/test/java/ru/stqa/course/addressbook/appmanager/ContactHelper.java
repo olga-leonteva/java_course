4972450f7b5e40d442b8139206a6489f64f52927
Olga Leonteva <leonteva.h@gmail.com>
2016-03-08 22:46:31 +0300

    Так же оптимизирован переход после создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ContactHelper.java
@@ -41,6 +41,9 @@ public class ContactHelper extends HelperBase {
      }
 
   public void returnToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home page"));
   }
 
