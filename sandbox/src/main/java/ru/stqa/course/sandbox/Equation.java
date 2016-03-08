adcd97d1ff2bce27f851fa7eabcab3a15fe54449
Olga Leonteva <leonteva.h@gmail.com>
2016-03-08 13:48:30 +0300

    Добавлены вложенные проверки для Equation и созданы новые тесты для них

--- sandbox/src/main/java/ru/stqa/course/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/Equation.java
@@ -11,7 +11,7 @@ public class Equation {
 
   private int n;
 
-  public Equation (double a, double b, double c) {
+  public Equation(double a, double b, double c) {
 
     this.a = a;
     this.b = b;
@@ -20,18 +20,24 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-      n = 2;
-    } else {
-      if (d == 0){
+    if (a != 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
       }
+    } else if (b != 0) {
+      n = 1;
+    } else if (c != 0) {
+          n = 0;
+        } else {
+        n = -1;
+      }
     }
-  }
 
-  public int rootNumber(){
+  public int rootNumber() {
     return n;
   }
 
