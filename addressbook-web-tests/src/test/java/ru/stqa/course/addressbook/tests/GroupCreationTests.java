99a4940ed5e5c06b6444fbd56fa113bb087a2f8c
Olga Leonteva <leonteva.h@gmail.com>
2016-04-18 19:06:07 +0300

    Дабавлено сравнения данных из базы для всех тестов для групп

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/GroupCreationTests.java
@@ -10,6 +10,7 @@ import ru.stqa.course.addressbook.model.Groups;
 
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -53,10 +54,11 @@ public class GroupCreationTests extends TestBase {
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
       app.goTo().groupPage();
-      Groups before = app.group().all();
+      Groups before = app.db().groups();
       app.group().create(group);
+      app.goTo().groupPage();
       assertThat(app.group().count(), equalTo(before.size() + 1));
-      Groups after = app.group().all();
+      Groups after = app.db().groups();
       assertThat(after, equalTo(before.withAdded(
               group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
@@ -65,11 +67,11 @@ public class GroupCreationTests extends TestBase {
   public void testBadGroupCreation() {
 
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData group = new GroupData().withName("test'");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
   }
 
