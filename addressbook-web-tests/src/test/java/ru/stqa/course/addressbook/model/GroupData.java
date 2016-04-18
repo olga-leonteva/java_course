91d144a532a3e7fb0a2eef15400c06852256d2ba
Olga Leonteva <leonteva.h@gmail.com>
2016-04-18 14:18:30 +0300

    Получение данных из базы для групп с помощью Object Relational Mapping (ORM)

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/model/GroupData.java
@@ -3,17 +3,35 @@ package ru.stqa.course.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @XStreamAlias("group")
+@Entity // объявляет класс GroupData привязанной к базе
+@Table(name = "group_list")
 
 public class GroupData {
   @XStreamOmitField   // пропустить поле ниже(не включать его в xml файл)
+  @Id
+  @Column(name = "group_id") //т.к. атрибут называется id, а таблица group_id => необходима доп.подсказка
   private int id = Integer.MAX_VALUE;
+
   @Expose // помечаем поля, которые должны быть добавлены в json
+  @Column(name = "group_name")
   private String name;
+
   @Expose
+  @Column(name = "group_header")
+  @Type(type = "text") //нужна подсказка
   private String header;
+
   @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text")
   private String footer;
 
   public int getId() {
