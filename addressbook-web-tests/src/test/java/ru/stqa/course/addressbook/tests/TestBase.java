6a4827e88cc590e3fad71d459b0c777ff56eca70
Olga Leonteva <leonteva.h@gmail.com>
2016-04-19 16:06:12 +0300

    Добавлены отключаемые проверки для UI для теста модификации группы

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.stqa.course.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +10,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.course.addressbook.appmanager.ApplicationManager;
+import ru.stqa.course.addressbook.model.GroupData;
+import ru.stqa.course.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by leonto on 3/2/2016.
@@ -41,4 +50,15 @@ public class TestBase {
   public void logTestStop(Method m){
     logger.info("Stop test " + m.getName());
   }
+
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
+
 }
