845cb60cbc09795d59dbde4fc933147260b4dcb4
Olga Leonteva <leonteva.h@gmail.com>
2016-04-22 18:25:02 +0300

    Начата реализация передачи файлов на удаленную машину

---
+++ mantis-tests/src/test/java/ru/stqa/cources/mantis/appmanager/FtpHelper.java
@@ -0,0 +1,39 @@
+package ru.stqa.cources.mantis.appmanager;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by leonto on 4/22/2016.
+ */
+public class FtpHelper {
+    private final ApplicationManager app;
+    private FTPClient ftp;
+
+    public FtpHelper(ApplicationManager app) {
+        this.app = app;
+        ftp = new FTPClient();
+    }
+
+    // заменяем файл
+    public void upload(File file, String target, String backup) throws IOException {
+        ftp.connect(app.getProperty("ftp.host"));
+        ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
+        ftp.deleteFile(backup);
+        ftp.rename(target, backup);
+        ftp.enterLocalPassiveMode();
+        ftp.storeFile(target, new FileInputStream(file)); // файл для чтения бинарных данных
+        ftp.disconnect();
+    }
+    // востанавлимаем конфигурацию
+    public void restore(String backup, String target) throws IOException {
+        ftp.connect(app.getProperty("ftp.host"));
+        ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
+        ftp.deleteFile(target);
+        ftp.rename(backup, target);
+        ftp.disconnect();
+    }
+}
